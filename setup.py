20f9af2000c90c19efc3f64a3add8be2b70dc377
Ben Hodgson <ben@benhodgson.com>
2010-07-18 16:58:20 +0100

    Adding description and appropriate keywords to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from distutils.core import setup
 
 setup(
-    name='Basin',
+    name='basin',
     version='0.1',
-    description='xxx',
-    packages=['basin'],
+    description='A Python module for generating string representations of '
+        'integers and bytestrings with arbitrary base and alphabet.',
+    py_modules=['basin'],
     url='http://github.com/benhodgson/basin',
     author='Ben Hodgson',
     author_email='ben@benhodgson.com',
-    keywords = ["base", "base64", "string"],
+    keywords = ['base', 'base32', 'base62', 'base64'],
     classifiers = [
-        "Programming Language :: Python",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: Public Domain",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
