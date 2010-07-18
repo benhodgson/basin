be3d9d1378afa496c97c8e091f972c1e9016e921
Ben Hodgson <ben@benhodgson.com>
2010-07-18 16:41:43 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='Basin',
+    version='0.1',
+    description='xxx',
+    packages=['basin'],
+    url='http://github.com/benhodgson/basin',
+    author='Ben Hodgson',
+    author_email='ben@benhodgson.com',
+    keywords = ["base", "base64", "string"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
