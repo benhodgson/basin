66ad373c6eb4d2746ebad5bc5f3bbece0868120e
Ben Hodgson <ben@benhodgson.com>
2010-07-18 21:14:00 +0100

    Added license info to setup.py; moved source to src/ directory

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from distutils.core import setup
 setup(
     name='basin',
     version='0.1',
+    license='Public Domain',
     description='A Python module for generating string representations of '
         'integers and bytestrings with arbitrary base and alphabet.',
+    package_dir = {'':'src'},
     py_modules=['basin'],
     url='http://github.com/benhodgson/basin',
     author='Ben Hodgson',
